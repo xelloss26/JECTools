9f47b66716442050a09b29244e06ee51db2f6a49
johndoe <john_doe@163.com>
2013-10-12 06:44:05 +0800

    add comment for github

--- src/com/bitstunnel/jectools/activator/Activator.java
+++ src/com/bitstunnel/jectools/activator/Activator.java
@@ -6,6 +6,7 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * add comment for github
  */
 public class Activator extends AbstractUIPlugin {
 
